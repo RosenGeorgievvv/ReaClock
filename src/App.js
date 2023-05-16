d365a7cec94ab5d11817af68ff65066a5061ff86
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-16 16:22:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
+  const [time, setTime] = useState('');
+
+  const formatTime = (value) => {
+    if (value < 10) {
+      return '0';
+    } else {
+      return '';
+    }
+    
+  }
   return (
     <div className="App">
-     <h1>Hello World</h1>
+      <h1>Hello World</h1>
     </div>
   );
 }
